ec169b15acac118361e8ed6c9690f9f2c2f0f7b8
Miguel Santos <miguelhenriqueaguiar7@gmail.com>
2025-10-21 16:02:14 -0300

    Refactor forward function and remove duplicate motors_stop
    Refactor forward function to include motors_stop and adjust velocity handling.

--- software/codebase/src/forward.c
+++ software/codebase/src/forward.c
@@ -1,22 +1,6 @@
 #include "motor.h"
 #include <pigpio.h>
 
-int forward(motor user_motor) {
-
-  motors_stop(user_motor);
-
-  if (user_motor.velocity > RANGE)
-    return 1;
-
-  gpioPWM(user_motor.left_motor[PWM], user_motor.velocity);
-  gpioPWM(user_motor.right_motor[PWM], user_motor.velocity);
-
-  gpioWrite(user_motor.left_motor[PIN_1], 1);
-  gpioWrite(user_motor.right_motor[PIN_1], 1);
-
-  return 0;
-}
-
 void motors_stop(motor user_motor) {
   gpioPWM(user_motor.left_motor[PWM], 0);
   gpioWrite(user_motor.left_motor[PIN_1], 0);
@@ -26,3 +10,24 @@ void motors_stop(motor user_motor) {
   gpioWrite(user_motor.right_motor[PIN_1], 0);
   gpioWrite(user_motor.right_motor[PIN_2], 0);
 }
+
+
+int forward(motor user_motor) {
+
+  motors_stop(user_motor);
+
+  if (user_motor.velocity > RANGE)
+     printf("O valor de velocidade escolhido e maior do que o valor máximo determinado.");
+    return 1;
+
+  gpioPWM(user_motor.left_motor[PWM], user_motor.velocity); // liga o PWM, assim como o gpioWrite liga os GPIOS. Posteriormente ele é desligado por motors_stop, que o seta para 0.
+  gpioPWM(user_motor.right_motor[PWM], user_motor.velocity);
+
+  gpioWrite(user_motor.left_motor[PIN_1], 1);
+  gpioWrite(user_motor.right_motor[PIN_1], 1);
+  usleep(user_motor.ftime);
+  
+  motors_stop(user_motor);
+  return 0;
+}
+
