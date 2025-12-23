3334887256eb1ae5065cba9fd12138536f2b8903
Miguel Santos <miguelhenriqueaguiar7@gmail.com>
2025-12-22 21:42:24 -0300

    Refactor QR code search logic and improve readability

--- software/codebase/camera/QR_search.cpp
+++ software/codebase/camera/QR_search.cpp
@@ -1,13 +1,11 @@
 #include "camera.hpp"
 
-#include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/objdetect.hpp> // QRCodeDetector
 #include <opencv2/videoio.hpp>
-#include <string.h>
 
-static std::string get_qrcode() {
+static std::string get_qrcode();
 
   // funcao que controla a movimentacao do robo enquanto le o QR code e retorna
   // a string lida.
@@ -16,32 +14,29 @@ static std::string get_qrcode() {
   int QR_search() {
 
     qr_code_data = "empty";
-
+    VideoCapture cam(0);
     spin(-999); // gira o robo indefinidamente pra esquerda
     distance_counter = 0;
 
-    while(distance_counter < ???? && (qr_code_data == "empty")){
+    while(distance_counter < 171 && (qr_code_data == "empty")){
       qr_code_data = get_qrcode();
 
       usleep(50000);
     }
 
-    if(distance_counter >= ????){
+    if(distance_counter >= 171){
       stop();
       std::cout
           << "\nNao foi possivel ler o QR Code. Movimentacao interrompida."
           << std::endl;
-      return 0;
+      exit(0);
     }
 
     std::cout << "\nQR Code lido: " << qr_code_data << std::endl;
 
     stop();
 
-    // agora voltar à direcao inicial para executar os comandos
-    // preciso passar os ticks contados para a funcao spin_by_ticks, mas uso o
-    // mesmo contador de distancia (distance_counter) para isso, entao salvo o
-    // valor em outra variavel temporaria
+
     int temp_distance = -distance_counter;
     spin_by_ticks(temp_distance); // gira o robo de volta à posicao inicial
 
@@ -53,9 +48,10 @@ static std::string get_qrcode() {
     return 0;
   }
 
+  using namespace cv;
+
   static std::string get_qrcode() {
 
-    VideoCapture cam(0);
     if (!cam.isOpened())
       return "erro ao abrir a camera";
 
@@ -68,5 +64,6 @@ static std::string get_qrcode() {
     std::vector<Point> bbox;
     std::string data = qr.detectAndDecode(frame, bbox);
 
-    (data.empty()) ? return "empty" : return data;
+    return data.empty() ? "empty" : data;
+    
   }
